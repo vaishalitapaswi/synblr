876cd0a2f789b67e3fbf44259c905bac27948190
vaishalitapaswi <28345849+vaishalitapaswi@users.noreply.github.com>
2019-07-01 12:41:05 +0530

    Update DeptManager.java

--- DeptManager.java
+++ DeptManager.java
@@ -8,13 +8,22 @@ import java.util.function.Predicate;
 public class DeptManager {
 	private List<Dept> list = new ArrayList<>();
 
-	public List<Dept> getList() {
-		return list;
+	public void create(Dept d){
+		list.add(d);
 	}
-	public void setList(List<Dept> list) {
-		this.list = list;
+	public List<Dept> list(){
+		return this.list;
 	}
 	
+	public void delete(int deptno){
+		for (int i = 0;i< list.size();i++) 
+		{
+			if (list.get(i).getDeptno() == deptno) {
+					list.remove(list.get(i));
+					break;
+			}
+		}
+	}
 	public void update(Dept newDept) {
 		for (int i = 0;i< list.size();i++) 
 		{
@@ -27,6 +36,23 @@ public class DeptManager {
 	}
 
 	public static void main(String[] args) {
-
+		DeptManager mgr = new DeptManager();
+		for(int i =1;i<=10;i++){
+			Dept d = new Dept();
+			d.setDeptno(i);
+			d.setDname("Nameof"+i);
+			if ((i % 2) ==0)
+				d.setLoc("BLR");
+			else
+				d.setLoc("Hyd");
+			mgr.create(d);
+		}
+		
+		mgr.delete(1);
+		Dept d= new Dept(2, "HR","Pnq");
+		mgr.update(d);
+		for (Dept  dept : mgr.list) {
+			System.out.println(dept);
+		}
 	}
 }
